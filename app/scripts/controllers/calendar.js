b6e94090f4a3da8ee05a9a5e74b840a8b5b4f18c
Lucy Schroeder <Lucy Schroeder>
2016-02-18 20:07:11 -0600

    new calendar, volunteer section

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -2,9 +2,9 @@
 
 /**
  * @ngdoc function
- * @name cctApp.controller:AboutCtrl
+ * @name cctApp.controller:CalendarCtrl
  * @description
- * # AboutCtrl
+ * # CalendarCtrl
  * Controller of the cctApp
  */
 angular.module('cctApp')
@@ -23,16 +23,18 @@ angular.module('cctApp')
             currentTimezone: 'America/Chicago' // an option!
     };
     /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+    $scope.showevents = [
+      {title: 'The Women',start: new Date(y, 2, 10, 19,0), end: new Date(y, 2, 10, 21,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 11, 19,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 12, 19,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 13, 14,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 17, 19,0), end: new Date(y, 2, 10, 21,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 18, 19,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 19, 19,0),allDay:false},
+      {title: 'The Women',start: new Date(y, 2, 20, 14,0),allDay:false}
     ];
 
-    /* event source that calls a function on every view switch */
+    /* event source that calls a function on every view switch 
     $scope.eventsF = function (start, end, timezone, callback) {
       var s = new Date(start).getTime() / 1000;
       var e = new Date(end).getTime() / 1000;
@@ -40,15 +42,22 @@ angular.module('cctApp')
       var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
       callback(events);
     };
-
-    $scope.calEventsExt = {
+*/
+    $scope.auditionevents = {
        color: '#f00',
        textColor: 'yellow',
        events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+          {title: 'Auditions',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+          {title: 'Auditions',start: new Date(y, m, 28),end: new Date(y, m, 29)}
         ]
+    };
+
+    $scope.workevents = {
+      color: 'green',
+      textColor: 'pink',
+      events: [
+        {title: 'Light Hang', start: new Date(y, 2, 5, 12, 30), allDay: false}
+      ]
     };
     /* alert on eventClick */
     $scope.alertOnEventClick = function( date, jsEvent, view){
@@ -107,7 +116,7 @@ angular.module('cctApp')
     /* config object */
     $scope.uiConfig = {
       calendar:{
-        height: 450,
+        height: 500,
         editable: true,
         header:{
           left: 'title',
@@ -122,6 +131,5 @@ angular.module('cctApp')
     };
 
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
-    $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
+    $scope.eventSources = [$scope.showevents, $scope.eventSource, $scope.auditionevents, $scope.workevents]; //$scope.eventsF
   });
